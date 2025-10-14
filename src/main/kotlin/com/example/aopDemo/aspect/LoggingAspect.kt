2fc65bc1ad73a18e2b598e69e2f9c35a7f9d90f4
asmitasharma9997 <asmitasharma@Asmitas-MacBook-Air.local>
2025-10-14 14:25:49 +0530

    pointcut expression

--- src/main/kotlin/com/example/aopDemo/aspect/LoggingAspect.kt
+++ src/main/kotlin/com/example/aopDemo/aspect/LoggingAspect.kt
@@ -8,6 +8,38 @@ import org.springframework.stereotype.Component
 @Component
 class LoggingAspect {
 
+    /*
+    * Point cut expressions
+    *  execution(
+    * modifier pattern ?
+    * return type pattern
+    * declaring type pattern ?
+    * method name pattern(param pattern)
+    * throws pattern?
+    * )
+    * ?-optional
+    * public void com.example.aopDemo.dao.AccountDao.addAddcount())
+    * modifier return type declaring methodName(param)
+    *
+    * starting with add
+    * void add*()
+    *
+    * any return type
+    * * add*()
+    *
+    * Parameter
+    * () -> no params
+    * (*)-> 1 argument of any type
+    * (..)-> 0 or more argument of any type
+    *
+    * Match on package
+    * * com.example.aopDemo.dao.*.*(..)
+    * return package. class.method (param)
+    *
+    *
+    *
+    *
+    * */
     @Before("execution(public void addAccount())")
     fun beforeAspect() {
         System.out.println("=====> Executing Before Advice <====== ")
